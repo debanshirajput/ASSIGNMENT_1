8626c6fdb16beeab923cdd7d25a82321fb26ac1c
debanshirajput <104027723+debanshirajput@users.noreply.github.com>
2022-06-09 22:36:06 +0530

    Add files via upload

---
+++ Output_ASSIGNMENT_3.py
@@ -0,0 +1,69 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/dr sanjeev/AppData/Local/Programs/Python/Python310/Assignment_3.py
+Enter the value:12
+The binary equivalent of value is: 1100
+
+Enter value 1: 12
+Enter value 2: 34
+Enter the desired mathematical expression( '+' or '-' or '*' or '/' ): +
+12  +  34  =  46
+Do you want to continue?(yes/no)yes
+
+Enter value 1: 13
+Enter value 2: 12
+Enter the desired mathematical expression( '+' or '-' or '*' or '/' ): -
+13  -  12  =  1
+Do you want to continue?(yes/no)yes
+
+Enter value 1: 12
+Enter value 2: 12
+Enter the desired mathematical expression( '+' or '-' or '*' or '/' ): *
+12  *  12  =  144
+Do you want to continue?(yes/no)yes
+
+Enter value 1: 12
+Enter value 2: 4
+Enter the desired mathematical expression( '+' or '-' or '*' or '/' ): /
+12  /  4  =  3.0
+Do you want to continue?(yes/no)no
+
+ a)The value is=  35
+
+Enter value for n: 12
+
+ b)The value is=  66.0
+
+Enter value for r: 12
+
+ c)The value is=  1809.5573684677208
+
+Enter value of r: 12
+Enter value of a: 13
+Enter value of b: 14
+
+ d)The value is=  3.4641016151377544
+
+Enter value of x1: 12
+Enter value of x2: 45
+Enter value of y1: 34
+Enter value of y2: 4
+
+ e)The value is=  -0.9090909090909091
+
+ a)
+0 1 2 3 4 
+ b)
+3 4 5 6 7 8 9 
+ c)
+4 7 10 
+ d)
+15 13 11 9 7 
+ e)
+
+No of hydorgen atom:10
+No of oxygen atom:4
+No of carbon atom20
+
+The total weight of the molecule is 314.291
