a025ceea24393b8033e7ce3af880b9617f98812d
debanshirajput <104027723+debanshirajput@users.noreply.github.com>
2022-07-25 18:28:54 +0530

    Add files via upload

---
+++ Output_ASSIGNMENT_6.py
@@ -0,0 +1,50 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/dr sanjeev/AppData/Local/Programs/Python/Python310/Assignment_6.py
+Enter a number: 23
+23  is not a perfect number
+
+Enter a String: mom
+mom is a palindrome
+
+Enter a number: 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+Enter a String: height
+No 'height' is not a panagram
+
+Enter a hyphen separated sentence: old-is-the-book
+book-is-old-the
+
+Student ID= 21102114
+Student ID= 21102114
+Student Name= Debanshi Rajput
+Student ID= 21102114
+Student Name= Debanshi Rajput
+Student Class= 1st Year
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+Enter number of elements : 6
+4
+5
+6
+7
+8
+9
+No triplets exist
+None
+Enter the sequence of parantheses : (){}[]
+(){}[] - is balanced
